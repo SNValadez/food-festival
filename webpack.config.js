f5a5662e07e0cc96b040803c6869acbc7a920262
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-18 09:57:56 -0800

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,11 @@
-const path = require("path");
+const path = require('path');
+
+
+module.exports = {
+  entry: './assets/js/script.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  mode: 'development'
+};
