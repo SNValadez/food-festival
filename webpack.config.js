79218e159e49937c456c3cc787d2206820f1f8b2
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-20 19:29:20 -0800

    webpack modue=;

--- webpack.config.js
+++ webpack.config.js
@@ -15,24 +15,27 @@ module.exports = {
   },
   module: {
     rules: [
-        {
-          test: /\.jpg$/i,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                esModule: false,
-                name (file) {
-                  return "[path][name].[ext]"
-                },
-                publicPath: function(url) {
-                  return url.replace("../", "/assets/")
-                }
-              }  
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name(file) {
+                return '[path][name].[ext]';
+              },
+              publicPath: function(url) {
+                return url.replace('../', '/assets/');
+              }
             }
-          ]
-        }
-      ]
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new webpack.ProvidePlugin({
